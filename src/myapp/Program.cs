fb840413f5db186960d863441ef0f2d307690ede
Genilson de Maceno <gmaceno@4bio.com.br>
2021-10-31 17:09:28 -0300

    Trabalhando com banco de dados

--- src/myapp/Program.cs
+++ src/myapp/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using myapp.Models;
 
 namespace myapp
 {
@@ -13,7 +15,26 @@ namespace myapp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+
+            using(var scope = host.Services.CreateScope()){
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                     SeedData.initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Um erro ocorreu no Seeding o DB");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
