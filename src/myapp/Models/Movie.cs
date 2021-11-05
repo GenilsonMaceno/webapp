b29ea128f29e19bade15d6171bf4baafe42a2501
Genilson de Maceno <gmaceno@4bio.com.br>
2021-11-04 21:21:47 -0300

    Adicionando validacao

--- src/myapp/Models/Movie.cs
+++ src/myapp/Models/Movie.cs
@@ -9,15 +9,24 @@ namespace myapp.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3), Required] // Combinando atribuitos
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        // [Display(Name = "Release Date")]
+        // Posso usar o DISPLAYFORMAT para especificar um tipo de data
+        // ApplyFormatInEditMode: especifica que a formatação também deve ser aplicada quando o valor é exibido em uma caixa de texto para edição.
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true), DataType(DataType.Date)] 
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(20)]
         public string Genre { get; set; }
 
+        [Range(1,100), DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(1), Required]
         public string Rating { get; set; }
     }
 }
